3359448f47a85c237ecd26735f58c6311ae18531
Nawseef <nawseefali@gmail.com>
2020-10-21 23:52:38 +0530

    New

--- rightJustify.swift
+++ rightJustify.swift
@@ -16,6 +16,26 @@ func rightjustify(s: String) {
     print(temp)
 }
 
+let closure = {
+    (s: String) -> String
+    in
+    var temp = s 
+
+    for _ in 1...70 {
+        if(temp.count<70) {
+            temp = " " + temp
+        }
+        else{
+            break;
+        }
+    }
+
+     return temp
+}
+
+func new(s: String) {
+    let justified = closure(s)
+    print(justified)
+}
+
 rightjustify(s: "monty")
-
-
--- song.swift
+++ song.swift
@@ -12,10 +12,10 @@ func song(number: inout Int) {
   
     else if number > 0 {
         print("""
-              \(number) bottles of water on the wall,
-              \(number) bottles of water,
-              ya’ take one down, ya’ pass it around,
-              \(number - 1) bottles of water on the wall.
+               \(number) bottles of water on the wall,
+               \(number) bottles of water,
+               ya’ take one down, ya’ pass it around,
+               \(number - 1) bottles of water on the wall.
 
                """)
 
@@ -25,5 +25,41 @@ func song(number: inout Int) {
 
 }
 
-var a = -2
+let closure = {
+    (number: Int) -> Bool
+    in
+    if number <= 0 {
+        return false
+    }
+
+    return true
+}
+
+func song1(number: inout Int) {
+    let bottleAvailable = closure(number)
+    if(bottleAvailable) {
+          print("""
+               \(number) bottles of water on the wall,
+               \(number) bottles of water,
+               ya’ take one down, ya’ pass it around,
+               \(number - 1) bottles of water on the wall.
+
+               """)
+
+          number -= 1
+          song(number: &number)
+    }
+    
+    else {
+        print("""
+              No bottles of water on the wall,
+              no bottles of water,
+              ya’ can’t take one down, ya’ can’t pass it around,
+              ’cause there are no more bottles of water on the wall!
+              """)
+        return;
+    }
+}
+
+var a = 3
 song(number: &a)
