2c49942f3fa5a3090931340930e924d147754f4b
Nawseef <nawseefali@gmail.com>
2020-10-21 23:54:28 +0530

    New

--- oddSum.swift
+++ oddSum.swift
@@ -36,42 +36,4 @@ func oddSum1(n: Int) -> Int {
     return 0
 }
 
-print(oddSum1(n: 25))func oddSum(n: Int) -> Int {
-    if n > 0 {
-        if(n % 2 == 1) {
-            return n + oddSum(n: n-1)
-        }
-        else {
-            return oddSum(n: n-1)
-        }
-    }
-    
-    return 0
- }
-
-
-let oddSumClosure = {
-    (n: Int) -> Bool
-    in
-
-    if(n % 2 == 1){
-        return true  
-    }
-
-    return false
-}
-
-func oddSum1(n: Int) -> Int {
-    let odd = oddSumClosure(n)
-    if n > 0 {
-        if(odd == true) {
-            return n + oddSum1(n: n-1)
-        }
-        else {
-            return oddSum1(n: n-1)
-        }
-    }
-    return 0
-}
-
 print(oddSum1(n: 25))
